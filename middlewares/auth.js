cbd99717d922a61c2ffea9da3100c2cf0c520e29
Saket Ray <phaser.84059@gmail.com>
2024-12-03 11:38:33 +0530

    Added admin check middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 import admin from '../firebase/index.js'
+import User from '../models/user.js'
 // Validate the user token and pass the userdata to controller (if token is valid)
 const authCheck = async (req, res, next) => {
     try{
@@ -13,7 +14,19 @@ const authCheck = async (req, res, next) => {
         })
     }
 }
-
+// Validate the admin user and pass the userdata to controller 
+const adminCheck = async (req, res, next) => {
+    const {email} = req.user;
+    const adminUser = await User.findOne({email}).exec()
+    if(adminUser && adminUser.role === 'admin'){
+        next()
+    }else{
+        res.status(403).json({
+            err : 'Admin resource. Access Denied'
+        })
+    }
+}
 export {
-    authCheck
+    authCheck,
+    adminCheck
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 import express from 'express'
 const router = express.Router()
 // middlewares
-import {authCheck} from '../middlewares/auth.js'
+import {authCheck, adminCheck} from '../middlewares/auth.js'
 // controllers
 import { createOrUpdateUser, currentUser } from '../controllers/auth.js'
 // route
 router.post('/create-or-update-user', authCheck, createOrUpdateUser)
 router.post('/current-user', authCheck, currentUser)
+router.post('/current-admin', authCheck, adminCheck, currentUser)
 
 // testMiddleware
 const testMiddleware = (req,res,next) => {
